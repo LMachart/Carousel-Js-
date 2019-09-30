52fbf0a966cafb7969fb7b4f0ed23ee83cc9f4dc
louis Machart <louis.machart19@gmail.com>
2019-09-30 11:38:00 +0200

    update for my project

--- script.js
+++ script.js
@@ -19,10 +19,39 @@ function nextImage() {
     li[position].classList.add('active');
 }
 
-    
-    
+function nextImageWithArrowKeyboard(event) {
+
+	if (event.keyCode === 39) {
+		nextImage();	
+	}
+}
+   
+
+function previousImage() {
+
+	position--;
+
+	if (position < 0) {
+		position = li.length-1;
+	}
+
+	for (let i = li.length-1; i >= 0 ;i--) {
+		li[i].classList.remove("active");
+	}
+
+	li[position].classList.add("active");
+}
+
+function previousImageWithArrowKeyboard(event) {
+	
+	if (event.keyCode === 37) {
+		previousImage();
+	}
+}
 
 
 
-buttonNext.addEventListener("click", nextImage);
+buttonNext.addEventListener("click", nextImage , );
 buttonPrevious.addEventListener("click", previousImage)
+document.addEventListener("keyup", nextImageWithArrowKeyboard);
+document.addEventListener("keyup", previousImageWithArrowKeyboard);
